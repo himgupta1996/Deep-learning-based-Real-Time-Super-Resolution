a1cec51d8cc9a8a61f05e87c0b721a7a7ef678e6
hgupta_umass_edu <hgupta_umass_edu@node39.maas>
2022-03-26 01:44:43 +0000

    Adding the VSR code for integration

---
+++ run_all_models.sh
@@ -0,0 +1,4 @@
+python ./codes/main.py --exp_dir ./experiments_BD/EGVSR/001 --mode test --model EGVSR --opt test.yml --gpu_id 0
+python ./codes/main.py --exp_dir ./experiments_BD/FRVSR/001 --mode test --model FRVSR --opt test.yml --gpu_id 0
+python ./codes/main.py --exp_dir ./experiments_BD/TecoGAN/001 --mode test --model TecoGAN --opt test.yml --gpu_id 0
+python ./codes/main.py --exp_dir ./experiments_BD/VESPCN/001 --mode test --model VESPCN --opt test.yml --gpu_id 0
